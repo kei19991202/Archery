7ff32426ad8cf0325b67aec391a336025fc21b9d
kazu824 <kazu824@outlook.com>
2016-10-01 20:26:15 +0900

    編集欄追加

--- mobile/src/main/java/android/kei1999/archery/MainActivity.java
+++ mobile/src/main/java/android/kei1999/archery/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText =(EditText)findViewById(R.id.editText);
+
     }
 
     public void todaysMenu(View v){
@@ -19,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent1);
 
     }
+
+    public void edit(View v){
+
+    }
+
 }
