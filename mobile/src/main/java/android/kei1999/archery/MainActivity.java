4528ba5b192325316e90fa3b1825d68f543b8168
kei19991202 <suzukikeiichirou@yahoo.co.jp>
2016-10-01 19:10:01 +0900

    Initial commit

---
+++ mobile/src/main/java/android/kei1999/archery/MainActivity.java
@@ -0,0 +1,22 @@
+package android.kei1999.archery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void todaysMenu(View v){
+
+        Intent intent1 = new Intent(this, TodaysMenu.class);
+        startActivity(intent1);
+
+    }
+}
---
+++ mobile/src/main/java/android/kei1999/archery/TodaysMenu.java
@@ -0,0 +1,16 @@
+package android.kei1999.archery;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TodaysMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todays_menu);
+    }
+
+}
