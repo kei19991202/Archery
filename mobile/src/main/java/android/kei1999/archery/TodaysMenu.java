9070eee58c17aa2b25aaaf79a198da772e426982
kei19991202 <suzukikeiichirou@yahoo.co.jp>
2016-10-03 00:57:01 +0900

    ボタン追加並びにIntent3追加

--- mobile/src/main/java/android/kei1999/archery/TodaysMenu.java
+++ mobile/src/main/java/android/kei1999/archery/TodaysMenu.java
@@ -7,15 +7,21 @@ import android.view.View;
 
 public class TodaysMenu extends AppCompatActivity {
 
+    int number1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todays_menu);
+
+        Intent intent3 = getIntent();
+        number1 = intent3.getIntExtra("num1",0);
     }
 
     public void morphisms(View v){
 
         Intent intent2 = new Intent(this, TheTotalNumberOfMorphisms.class);
+        intent2.putExtra("num1",number1);
         startActivity(intent2);
 
     }
