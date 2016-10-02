5bab341ab6da3979e97e1968700156271790eea5
kei19991202 <suzukikeiichirou@yahoo.co.jp>
2016-10-03 00:57:48 +0900

    bug部分を解決

--- mobile/src/main/java/android/kei1999/archery/TheTotalNumberOfMorphisms.java
+++ mobile/src/main/java/android/kei1999/archery/TheTotalNumberOfMorphisms.java
@@ -1,7 +1,6 @@
 package android.kei1999.archery;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentTransitionCompat21;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
