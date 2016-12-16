ddf638d5ee9469c6d05d5c37c9ef4ebc0e766c93
kei19991202 <suzukikeiichirou@yahoo.co.jp>
2016-12-16 14:37:27 +0900

    色々,

--- mobile/src/main/java/android/kei1999/archery/PointActivity.java
+++ mobile/src/main/java/android/kei1999/archery/PointActivity.java
@@ -2,16 +2,22 @@ package android.kei1999.archery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PointActivity extends AppCompatActivity {
+
+
+    int num[] = new int[43];
+
+    TextView tv[] = new TextView[43];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_point);
+
 
     }
-
-
 
 }
